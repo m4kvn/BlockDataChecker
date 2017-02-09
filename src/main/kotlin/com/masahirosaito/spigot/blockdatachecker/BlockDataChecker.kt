0a46621cbc280dda1e954d0a87e9832a97be3a61
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-09 22:24:40 +0900

    Kotlin 対応

---
+++ src/main/kotlin/com/masahirosaito/spigot/blockdatachecker/BlockDataChecker.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.blockdatachecker
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BlockDataChecker : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

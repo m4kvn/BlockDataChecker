e2deb654025a5bbe87409932ad79161e5fe0d618
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-09 22:10:55 +0900

    Initial Gradle Project

---
+++ src/main/java/com/masahirosaito/spigot/blockdatachecker/BlockDataChecker.java
@@ -0,0 +1,17 @@
+package com.masahirosaito.spigot.blockdatachecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockDataChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
